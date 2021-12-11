ed06d993d6eaae4a3c1c55e78784f966e78feb18
sonia <sonianr03@gmail.com>
2021-12-11 18:51:00 +0100

    puyta

---
+++ block_folder/grass.py
@@ -0,0 +1,9 @@
+from block import Block
+
+
+class Grass(Block):
+    # This class contains the attributes and the functions for the grass
+
+    def __init__(self, x_position: int, y_position: int, image_bank: int, image_x: int, image_y: int,
+                 width: int, height: int, breakable: bool):
+        super().__init__(x_position, y_position, image_bank, image_x, image_y, width, height, breakable)
