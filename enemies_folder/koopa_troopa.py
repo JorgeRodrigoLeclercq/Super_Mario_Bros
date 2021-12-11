3892667e3b4675902f3647f2df186e1d1654ad41
sonia <sonianr03@gmail.com>
2021-12-11 18:52:44 +0100

    puyta

---
+++ enemies_folder/koopa_troopa.py
@@ -0,0 +1,8 @@
+from enemies import Enemies
+
+
+class KoopaTroopa(Enemies):
+    # This class contains the specific attributes and functions for Koopa Troopa
+
+    def __init__(self, x_position: int, y_position: int, image_bank: int, image_x: int, image_y: int, width: int, height: int, direction: bool, dead: bool):
+        super().__init__(x_position, y_position, image_bank, image_x, image_y, width, height, direction, dead)
---
+++ special_objects/mushroom.py
@@ -0,0 +1,8 @@
+from special_objects import SpecialObjects
+
+class Mushroom(SpecialObjects):
+    # This class contains the attributes and the functions for the cleared blocks_folder
+
+    def __init__(self, x_position: int, y_position: int, image_bank: int, image_x: int, image_y: int,
+                 width: int, height: int, c: bool):
+        super().__init__(x_position, y_position, image_bank, image_x, image_y, width, height, c)
