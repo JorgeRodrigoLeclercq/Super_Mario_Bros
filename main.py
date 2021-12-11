27e15de2936694fd931ba1e255c2b1301bb977fe
SoniaNR03 <95230555+SoniaNR03@users.noreply.github.com>
2021-12-11 18:42:14 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import pyxel
+from board import Board
+
+# Basic information
+width = 256
+height = 256
+floor_length = 2048
+
+# Creates the screen
+pyxel.init(width, height, caption="MarioBros")
+
+# Loads the assets
+pyxel.load("assets\marioassets.pyxres")
+
+# Execute the board
+mario_board = Board(width, height, floor_length)
+pyxel.run(mario_board.update, mario_board.draw)
